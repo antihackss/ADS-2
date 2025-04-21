e9da986498da5610afc97bb02388728ce8305266
antihackss <vladmakov0859@gmail.com>
2025-04-21 22:39:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,20 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
+#include <cmath>
 
 double pown(double value, uint16_t n) {
   double result = 1.0;
-    for (uint16_t i = 0; i < n; ++i) {
-        result *= value;
-    }
-    return result;
+  for (uint16_t i = 0; i < n; ++i)
+    result *= value;
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
   uint64_t result = 1;
-    for (uint16_t i = 2; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
+  for (uint16_t i = 2; i <= n; ++i)
+    result *= i;
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
@@ -24,31 +22,26 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-  double sum = 0.0;
-    double term = 1.0;
-    for (uint16_t n = 0; n < count; ++n) {
-        sum += term;
-        term *= x / (n + 1);
-    }
-    return sum;
+  double result = 0.0;
+  for (uint16_t n = 0; n < count; ++n)
+    result += calcItem(x, n);
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-  double sum = 0.0;
-    double term = x;
-    for (uint16_t n = 0; n < count; ++n) {
-        sum += term;
-        term *= -x * x / ((2*n + 2) * (2*n + 3));
-    }
-    return sum;
+  double result = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    double term = calcItem(-1.0, n) * calcItem(x, 2 * n + 1);
+    result += term;
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  double sum = 0.0;
-    double term = 1.0;
-    for (uint16_t n = 0; n < count; ++n) {
-        sum += term;
-        term *= -x * x / ((2*n + 1) * (2*n + 2));
-    }
-    return sum;
+  double result = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    double term = calcItem(-1.0, n) * calcItem(x, 2 * n);
+    result += term;
+  }
+  return result;
 }
