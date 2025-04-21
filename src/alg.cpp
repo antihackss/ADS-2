02ca7a2baf7709985ba0ac718cfe524c576d3763
antihackss <vladmakov0859@gmail.com>
2025-04-21 22:57:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,19 +12,22 @@ double pown(double value, uint16_t n) {
 
 uint64_t fact(uint16_t n) {
   uint64_t result = 1;
-  for (uint16_t i = 2; i <= n; ++i)
+  for (uint16_t i = 1; i <= n; ++i)
     result *= i;
   return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x, n) / static_cast<double>(fact(n));
+  n = pown(x, n);
+  d = fact(n);
+  return n / d;
 }
 
 double expn(double x, uint16_t count) {
   double result = 0.0;
-  for (uint16_t n = 0; n < count; ++n)
+  for (uint16_t n = 0; n <= count; ++n) {
     result += calcItem(x, n);
+  }  
   return result;
 }
 
